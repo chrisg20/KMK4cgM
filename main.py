fe006bdad4191433636c3448440a94fa84a8d603
chrisg20 <31445708+chrisg20@users.noreply.github.com>
2022-12-08 23:18:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-print("Starting")
-
 from kb import SeeedBoard
 from kmk.keys import KC
+from kmk.handlers.sequences import send_string
+from kmk.extensions.RGB import RGB
+from kmk.modules.layers import Layers
+from kmk.modules.midi import MidiKeys
 
 keyboard = SeeedBoard()
 
+rgb_ext = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=keyboard.rgb_num_pixels, hue_default=0, sat_default=255, val_default=255)
+keyboard.extensions = [rgb_ext]
+keyboard.modules = [Layers(), MidiKeys()]
+
 keyboard.keymap = [
-    [KC.RGB_TOG, KC.RGB_HUD, KC.RGB_HUI,
-     KC.RGB_VAI, KC.RGB_VAD, KC.RGB_MODE_SWIRL,
-     KC.RGB_ANI, KC.RGB_AND, KC.RGB_MODE_KNIGHT,
-     
-     
-     
-     ]
+
+    [KC.MEDIA_PREV_TRACK, KC.MEDIA_PLAY_PAUSE, KC.MEDIA_NEXT_TRACK,
+	KC.AUDIO_VOL_DOWN, KC.AUDIO_MUTE, KC.AUDIO_VOL_UP,
+	KC.A, KC.B, KC.MO(1)
+
+    ],
+
+    [KC.RGB_HUI, KC.RGB_SAI, KC.RGB_VAI,
+	KC.RGB_HUD, KC.RGB_SAD, KC.RGB_VAD,
+	KC.RGB_MODE_PLAIN, KC.RGB_MODE_SWIRL, KC.NO
+
+    ],
+
+    [KC.N1, KC.N2, KC.N3,
+	KC.N4, KC.N5, KC.N6,
+	KC.N7, KC.MO(0), KC.N9
+
+    ],
+
+    [KC.N1, KC.N2, KC.N3,
+	KC.N4, KC.N5, KC.N6,
+	KC.N7, KC.N8, KC.N9
+
+    ]
 ]
 
-# keyboard.keymap = [
-#     [KC.NUMPAD_1, KC.NUMPAD_2, KC.NUMPAD_3,
-#      KC.NUMPAD_4, KC.NUMPAD_5, KC.NUMPAD_6,
-#      KC.NUMPAD_7, KC.NUMPAD_8, KC.NUMPAD_9,
-#      
-#      
-#      
-#      ]
-# ]
+if __name__ == '__main__': keyboard.go()
 
-if __name__ == '__main__':
-    keyboard.go()
